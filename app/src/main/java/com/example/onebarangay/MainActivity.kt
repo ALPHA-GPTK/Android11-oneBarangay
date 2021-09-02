c77ed097ce73408dedb67a11b7337b3bd7d1f54c
PrynsTag <princevelasco16@gmail.com>
2021-09-02 18:15:08 +0800

    feat: add sentry sdk

--- app/src/main/java/com/example/onebarangay/MainActivity.kt
+++ app/src/main/java/com/example/onebarangay/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.onebarangay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.sentry.Sentry
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
