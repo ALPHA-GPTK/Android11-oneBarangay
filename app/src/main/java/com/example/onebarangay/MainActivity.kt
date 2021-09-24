4b0d0321d2547c618661d769af5411e9626f090a
Katherine Mariñas <katherinemarinas11@gmail.com>
2021-09-24 14:42:51 +0800

    Navigation

--- app/src/main/java/com/example/onebarangay/MainActivity.kt
+++ app/src/main/java/com/example/onebarangay/MainActivity.kt
@@ -3,49 +3,83 @@ package com.example.onebarangay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import fragments.DashboardFragment
-import fragments.NotificationsFragment
-import fragments.ProfileFragment
-import fragments.ServicesFragment
-import io.sentry.Sentry
+import com.example.onebarangay.fragments.DashboardFragment
+import com.example.onebarangay.fragments.NotificationsFragment
+import com.example.onebarangay.fragments.ProfileFragment
+import com.example.onebarangay.fragments.ServicesFragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val dashboardFragment = DashboardFragment()
-    private val servicesFragment = ServicesFragment()
-    private val notificationFragment = NotificationsFragment()
-    private val profileFragment = ProfileFragment()
+//    private val dashboardFragment = DashboardFragment()
+//    private val servicesFragment = ServicesFragment()
+//    private val notificationFragment = NotificationsFragment()
+//    private val profileFragment = ProfileFragment()
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_dashboard -> {
+                moveToFragment(DashboardFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_services -> {
+                moveToFragment(ServicesFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notification -> {
+                moveToFragment(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                moveToFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        replaceFragment(dashboardFragment)
-
-        bottom_navigation.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.dashboard -> replaceFragment(dashboardFragment)
-                R.id.services -> replaceFragment(servicesFragment)
-                R.id.notification -> replaceFragment(notificationFragment)
-                R.id.profile -> replaceFragment(profileFragment)
-            }
-            true
-        }
+//        replaceFragment(dashboardFragment)
+//
+//        bottom_navigation.setOnNavigationItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.dashboard -> replaceFragment(dashboardFragment)
+//                R.id.services -> replaceFragment(servicesFragment)
+//                R.id.notification -> replaceFragment(notificationFragment)
+//                R.id.profile -> replaceFragment(profileFragment)
+//            }
+//            true
+//        }
 
 //        val adapter = ItemAdapter()
 //        announcementRV.layoutManager = LinearLayoutManager(this)
 //        announcementRV.adapter = adapter
 
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        moveToFragment(DashboardFragment())
+
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        if (fragment != null) {
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, fragment)
-            transaction.commit()
-        }
+    private fun moveToFragment(fragment: Fragment) {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
     }
+
+//    private fun replaceFragment(fragment: Fragment) {
+//        if (fragment != null) {
+//            val transaction = supportFragmentManager.beginTransaction()
+//            transaction.replace(R.id.fragment_container, fragment)
+//            transaction.commit()
+//        }
+//    }
 }
--- app/src/main/java/com/example/onebarangay/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/onebarangay/fragments/ProfileFragment.kt
@@ -1,41 +1,36 @@
-package fragments
+package com.example.onebarangay.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.onebarangay.ItemAdapter
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.example.onebarangay.R
-import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class DashboardFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val items = listOf("Male", "Female")
+        val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_gender, items)
+        (gendDrop.editText as? AutoCompleteTextView)?.setAdapter(adapter)
 
-
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-//        val adapter = ItemAdapter()
-//        announcementRV.layoutManager = LinearLayoutManager(this)
-//        announcementRV.adapter = adapter
     }
 
 }
