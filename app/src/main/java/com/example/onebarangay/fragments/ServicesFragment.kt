b610f1bf1ed8caf71138a0e24d2e8f48ea85da21
Katherine Mariñas <katherinemarinas11@gmail.com>
2021-09-30 03:56:33 +0800

    Services navigation

--- app/src/main/java/com/example/onebarangay/fragments/ServicesFragment.kt
+++ app/src/main/java/com/example/onebarangay/fragments/ServicesFragment.kt
@@ -1,11 +1,16 @@
 package com.example.onebarangay.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.onebarangay.R
+import com.example.onebarangay.ScanDocumentActivity
+import com.example.onebarangay.ViewAppointmentActivity
+import com.example.onebarangay.ViewReportsActivity
+import kotlinx.android.synthetic.main.fragment_services.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +22,23 @@ class ServicesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false)
+        val view = inflater.inflate(R.layout.fragment_services, container, false)
+
+        view.vAppointmentBtn.setOnClickListener {
+            startActivity(Intent(context, ViewAppointmentActivity::class.java))
+        }
+
+        view.vReportsBtn.setOnClickListener {
+            startActivity(Intent(context, ViewReportsActivity::class.java))
+
+        }
+
+        view.sDocuBtn.setOnClickListener {
+            startActivity(Intent(context, ScanDocumentActivity::class.java))
+
+        }
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
