6bfb89a87674893ad6db263da50459460215bef6
Katherine Mariñas <katherinemarinas11@gmail.com>
2021-10-13 19:38:35 +0800

    Announcement in recyclerview

---
+++ app/src/main/java/com/example/onebarangay/Model.kt
@@ -0,0 +1,5 @@
+package com.example.onebarangay
+
+class Model(val title: String, val date: String, val time: String, val desc: String, val image: Int) {
+
+}
--- app/src/main/java/com/example/onebarangay/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/onebarangay/fragments/DashboardFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.onebarangay.AnnouncementAdapter
+import com.example.onebarangay.Model
 import com.example.onebarangay.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,22 +23,23 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+//        val recyclerView:RecyclerView
+//
+//        recyclerView = view.findViewById(R.id.rv_announcement)
+//        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        val adapter = ItemAdapter()
-//        announcementRV.layoutManager = LinearLayoutManager(this)
-//        announcementRV.adapter = adapter
     }
 
 }
